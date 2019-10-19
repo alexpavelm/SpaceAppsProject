06d219c7bf23a6ee22135cff2d364b5f537d39ce
XhakaLaca <erhanmamut@tahoo.com>
2019-10-19 20:01:59 +0300

    Class for static var.

--- lib/LocationData.dart
+++ lib/LocationData.dart
@@ -5,14 +5,14 @@ import 'PingData.dart';
 class LocationData {
 
   String city;
-  String coutry;
+  String country;
   String weather;
   int temperature;
   int quality;
   List<PingData> pings;
 
 
-  LocationData(this.city, this.coutry, this.weather, this.temperature, this.quality, this.pings);
+  LocationData(this.city, this.country, this.weather, this.temperature, this.quality, this.pings);
 
 
 
