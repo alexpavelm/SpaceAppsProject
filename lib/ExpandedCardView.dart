7d954d5ef8299d43b6575b62c9a6fbba2dba346e
criss11 <cristina.ivanov@stud.acs.upb.ro>
2019-10-19 23:12:28 +0300

    card

--- lib/ExpandedCardView.dart
+++ lib/ExpandedCardView.dart
@@ -1,25 +1,60 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'GlobalData.dart';
 import 'LocationData.dart';
 
 class ExpandedCardView extends StatelessWidget {
   final LocationData data;
+  var globalData = GlobalData();
+
   ExpandedCardView(this.data);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         iconTheme: IconThemeData(
-        color: Colors.grey, //change your color here
-    ),
-    title: Text(
-    data.city,
-    ),
-    backgroundColor: Colors.transparent,
-    elevation: 0,
-    ),
-    body: Container(),
+          color: Colors.grey, //change your color here
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Center(
+          child: Column(
+            children: <Widget>[
+              Text(data.city,
+                  style: TextStyle(
+                      fontFamily: 'Raleway',
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold
+                  )),
+              Padding(
+                padding: EdgeInsets.only(top: 6, bottom: 35),
+                child: Text(data.country,
+                    style: TextStyle(
+                        fontFamily: 'Raleway',
+                        fontSize: 17,
+                        color: Colors.black87
+                    )),
+              ),
+              Container(
+                height: 120,
+                color: globalData.getCardColor(data.quality),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 40, right: 40),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(globalData.getQualityIcon(data.quality), size: 50, color: Colors.black.withOpacity(0.6),),
+
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+
+      ),
     );
   }
 }
