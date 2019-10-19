d06ea6e053f0da4e0109742dfb4d354225fa4f9e
predandrada <predandrada@yahoo.ro>
2019-10-20 02:38:30 +0300

    Merge branch 'master' of https://github.com/alexpavelm/SpaceAppsProject
    # Conflicts:
    #	lib/GlobalData.dart

--- lib/CurrentLocation.dart
+++ lib/CurrentLocation.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:space_apps_project/SmallCard.dart';
-import 'package:space_apps_project/WeatherData.dart';
-
 import 'GlobalData.dart';
-import 'LocationData.dart';
 
 class CurrentLocation extends StatefulWidget {
   @override
@@ -28,11 +24,10 @@ class CurrentLocationState extends State<CurrentLocation> {
             child: Padding(
               padding: const EdgeInsets.all(5.0),
               child: Container(
-                height: 330,
+                height: 350,
                 child: Column(
                   children: <Widget>[
                     Container(
-//                      color: Colors.lightBlue,
                       height: 150,
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -53,134 +48,168 @@ class CurrentLocationState extends State<CurrentLocation> {
                                 style: TextStyle(
                                   fontFamily: 'Raleway',
                                 ),
-                              )
+                              ),
                             ],
-                          )
+                          ),
                         ],
                       ),
                     ),
                     Container(
-//                      color: Colors.grey,
+//                      color:
+//                          globalData.getCardColor(globalData.mainCity.quality),
                       height: 80,
-                      child: Row(
+                      child: Card(
+                        color:globalData.getCardColor(globalData.mainCity.quality),
+                        child: Row(
 //                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                        crossAxisAlignment: CrossAxisAlignment.center,
+//                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
 //                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(left: 20),
-                            child: Icon(
-                              globalData.getQualityIcon(
-                                  globalData.mainCity.quality),
-                              size: 50,
-                              color: Colors.black.withOpacity(0.6),
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(left: 22, bottom: 5),
+                              child: Icon(
+                                globalData
+                                    .getQualityIcon(globalData.mainCity.quality),
+                                size: 60,
+                                color: Colors.black.withOpacity(0.6),
+                              ),
                             ),
-                          ),
-                          Text(
-                            globalData.getQualityText(globalData.mainCity.quality),
-                            style: TextStyle(
-                              fontFamily: 'Raleway',
-                              fontSize: 30,
+                            Padding(
+                              padding: const EdgeInsets.only(left: 47),
+                              child: Text(
+                                globalData
+                                    .getQualityText(globalData.mainCity.quality),
+                                style: TextStyle(
+                                  fontFamily: 'Raleway',
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                             ),
-                          ),
-                          Text(
-                            "Air Quality Today: " + globalData.mainCity.quality.toString(),
-                            style: TextStyle(
-                              fontFamily: 'Raleway',
-                              fontSize: 15,
+                            Padding(
+                              padding: const EdgeInsets.only(left: 44, top: 15),
+                              child: Text(
+                                "AQI: ",
+                                style: TextStyle(
+                                  fontFamily: 'Raleway',
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                             ),
-                          ),
-                        ],
+                            Padding(
+                              padding: const EdgeInsets.only(right: 25),
+                              child: Text(
+                                globalData.mainCity.quality.toString(),
+                                style: TextStyle(
+                                  fontFamily: 'Raleway',
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                     Container(
+//                      color:
+//                          globalData.getCardColor(globalData.mainCity.quality),
                       height: 100,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                          Container(
-                            width: 72,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                getWeatherIcon(
-                                    globalData.weatherData.weather[0]),
-                                Text(
-                                  globalData.weatherData.temperature[0]
-                                          .toString() +
-                                      " °C",
-                                  textScaleFactor: 2,
-                                ),
-                              ],
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Container(
+                              width: 72,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Text("date"),
+                                  getWeatherIcon(
+                                      globalData.weatherData.weather[0]),
+                                  Text(
+                                    globalData.weatherData.temperature[0]
+                                            .toString() +
+                                        " °C",
+                                    textScaleFactor: 2,
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                          Container(
-                            width: 72,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                getWeatherIcon(
-                                    globalData.weatherData.weather[1]),
-                                Text(
-                                  globalData.weatherData.temperature[1]
-                                          .toString() +
-                                      " °C",
-                                  textScaleFactor: 2,
-                                ),
-                              ],
+                            Container(
+                              width: 72,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Text("date"),
+                                  getWeatherIcon(
+                                      globalData.weatherData.weather[1]),
+                                  Text(
+                                    globalData.weatherData.temperature[1]
+                                            .toString() +
+                                        " °C",
+                                    textScaleFactor: 2,
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                          Container(
-                            width: 72,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                getWeatherIcon(
-                                    globalData.weatherData.weather[2]),
-                                Text(
-                                  globalData.weatherData.temperature[2]
-                                          .toString() +
-                                      " °C",
-                                  textScaleFactor: 2,
-                                ),
-                              ],
+                            Container(
+                              width: 72,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Text("date"),
+                                  getWeatherIcon(
+                                      globalData.weatherData.weather[2]),
+                                  Text(
+                                    globalData.weatherData.temperature[2]
+                                            .toString() +
+                                        " °C",
+                                    textScaleFactor: 2,
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                          Container(
-                            width: 72,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                getWeatherIcon(
-                                    globalData.weatherData.weather[3]),
-                                Text(
-                                  globalData.weatherData.temperature[3]
-                                          .toString() +
-                                      " °C",
-                                  textScaleFactor: 2,
-                                ),
-                              ],
+                            Container(
+                              width: 72,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Text("date"),
+                                  getWeatherIcon(
+                                      globalData.weatherData.weather[3]),
+                                  Text(
+                                    globalData.weatherData.temperature[3]
+                                            .toString() +
+                                        " °C",
+                                    textScaleFactor: 2,
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                          Container(
-                            width: 72,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                getWeatherIcon(
-                                    globalData.weatherData.weather[4]),
-                                Text(
-                                  globalData.weatherData.temperature[4]
-                                          .toString() +
-                                      " °C",
-                                  textScaleFactor: 2,
-                                ),
-                              ],
+                            Container(
+                              width: 72,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Text("date"),
+                                  getWeatherIcon(
+                                      globalData.weatherData.weather[4]),
+                                  Text(
+                                    globalData.weatherData.temperature[4]
+                                            .toString() +
+                                        " °C",
+                                    textScaleFactor: 2,
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ],
