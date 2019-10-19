7d39ec28ead54fd702538ec29bae9fb868c49551
predandrada <predandrada@yahoo.ro>
2019-10-19 20:23:42 +0300

    another useless update

--- lib/SmallCard.dart
+++ lib/SmallCard.dart
@@ -97,7 +97,7 @@ class _SmallCardState extends State<SmallCard> {
                       children: <Widget>[
                         getWeatherIcon(weather),
                         Text(
-                          temperature.toString() + "°C",
+                          temperature.toString() + "*C",
                           style: TextStyle(fontSize: 25, fontFamily: 'Raleway'),
                         ),
                       ],
