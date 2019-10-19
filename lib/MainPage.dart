abe671fb41c4f04b3b40813d31d9b4e4be77b5eb
alexz <prods.alex@gmail.com>
2019-10-19 19:12:19 +0300

    Merge branch 'master' of https://github.com/alexpavelm/SpaceAppsProject
    # Conflicts:
    #	lib/main.dart

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:space_apps_project/CurrentLocation.dart';
 
 import 'FunFact.dart';
+import 'SmallCard.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -12,17 +13,13 @@ class _MainPageState extends State<MainPage> {
   final List<Widget> _children = [
     FunFact(),
     CurrentLocation(),
+    SmallCard()
   ];
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: _children,
     );
-  }
-
-  List<Widget> getList() {
-    _children.addAll([]);
-    return _children;
   }
 
 
