4222f25183667b4a78c257cf600236480893b41f
criss11 <ivanov_christina@yahoo.com>
2019-10-20 11:55:50 +0300

    info

--- lib/InfoTab.dart
+++ lib/InfoTab.dart
@@ -27,7 +27,7 @@ class InfoTabState extends State<InfoTab> {
         Image.asset('assets/airquality.jpg'),
         infoCard(FontAwesomeIcons.chartLine,
             '  Accuracy of AQI',
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"),
+            "All of our data is calculated by taking into account all the different factors that influence air pollution on a daily basis. We extract our data from APIs and calculate the air quality index with an accuracy of 77,78%, using MATLAB."),
       ],
     ));
   }
